2a94fc0161608359edb90badf9a19fe3d1177c96
Szeksick <konradgugala@gmail.com>
2019-04-02 21:32:28 +0200

    Reading data from txt file and displaying in textarea

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,59 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Controller {
-}
+@FXML private TextArea result;
+@FXML private AnchorPane ap;
+private ArrayList<Float> tabx, taby, templist;
+
+
+    public void calculate(ActionEvent e) {
+        Stage stage = (Stage) ap.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile == null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Błąd kalkulatora");
+            alert.setHeaderText(null);
+            alert.setContentText("Nie wybrano pliku");
+            alert.showAndWait();
+        } else {
+            try {
+                templist = new ArrayList<Float>();
+                try (Scanner scanner = new Scanner(selectedFile)) {
+                        while (scanner.hasNext())
+                            templist.add(Float.parseFloat(scanner.next()));
+                    } catch (FileNotFoundException fileexept) {
+                        fileexept.printStackTrace();
+                }
+                tabx = new ArrayList<Float>(templist.subList(0, (templist.size()/2)));
+                taby = new ArrayList<Float>(templist.subList(templist.size()/2,templist.size()));
+                templist.clear();
+
+                result.setText("x="+String.valueOf(tabx)+'\n'+"y="+String.valueOf(taby));
+            } catch (Exception exept) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Błąd kalkulatora");
+                alert.setHeaderText(null);
+                alert.setContentText("Niewłaściwy plik");
+                alert.showAndWait();
+                }
+            }
+        }
+    }
+
+
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Konrad Gugała gr 11A - Interpolacja metodą Newtona");
+        primaryStage.setScene(new Scene(root, 400, 275));
         primaryStage.show();
+
     }
 
 
